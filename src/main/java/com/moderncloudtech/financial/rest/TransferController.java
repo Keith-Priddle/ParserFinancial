79a042f8d2f190cf561cee3c1015a8d91a94b1ba
keithpriddle <k.priddle66@gmail.com>
2020-02-06 11:32:27 +0000

    test mysql container

--- src/main/java/com/moderncloudtech/financial/rest/TransferController.java
+++ src/main/java/com/moderncloudtech/financial/rest/TransferController.java
@@ -20,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.moderncloudtech.financial.accounts.Transfer;
 import com.moderncloudtech.financial.services.TransferServices;
 
 

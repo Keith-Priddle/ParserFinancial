db1a4564ad49b5131c1809c6204001a9748a3c2d
keithpriddle <k.priddle66@gmail.com>
2020-02-06 07:18:31 +0000

    T

--- src/main/java/com/moderncloudtech/financial/ParserFinancialApplication.java
+++ src/main/java/com/moderncloudtech/financial/ParserFinancialApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -15,7 +16,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootApplication(scanBasePackages = {"com.moderncloudtech.financial"})
 public class ParserFinancialApplication {
+	@RequestMapping("/")
+	public String Home() {
+		
+		return " Hello Docker World!!";
+	}
 
+	
 	public static void main(String[] args) {
 		System.setProperty("server.servlet.context-path", "/");
 		SpringApplication.run(ParserFinancialApplication.class, args);

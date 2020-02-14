8bbe4cb311516bb032c9a37a89626a3d82cb3bb2
keithpriddle <k.priddle66@gmail.com>
2020-02-14 09:53:05 +0000

    t

--- src/main/java/com/moderncloudtech/financial/ParserFinancialApplication.java
+++ src/main/java/com/moderncloudtech/financial/ParserFinancialApplication.java
@@ -1,16 +1,8 @@
 package com.moderncloudtech.financial;
 
-import java.io.IOException;
-import java.net.URL;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.client.RestTemplate;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 
 

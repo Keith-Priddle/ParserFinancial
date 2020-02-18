34da81300427c6e1c20ba29136b9dbea53009fe9
keithpriddle <k.priddle66@gmail.com>
2020-02-18 16:16:20 +0000

    acl

--- src/main/java/com/moderncloudtech/financial/rest/AccountHolderController.java
+++ src/main/java/com/moderncloudtech/financial/rest/AccountHolderController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -26,7 +27,7 @@ public class AccountHolderController {
 		super();
 		this.accountHolderServices = accountHolderServices;
 	}
-	
+	@CrossOrigin(origins="http://localhost:4200")
 	@RequestMapping(path="/AccountHolder", method=RequestMethod.GET, produces="application/json")
 	public ResponseEntity<List<AccountHolder>> getAccountHolders(){
 		System.out.println("Hitting Account Holder Controller---------------->");
@@ -34,7 +35,7 @@ public class AccountHolderController {
 		return new ResponseEntity<List<AccountHolder>>(accountHolders, HttpStatus.OK);
 	}
 	
-	
+	@CrossOrigin(origins="http://localhost:4200")
 	@RequestMapping(path="/AccountHolder", method=RequestMethod.POST, consumes="application/json", produces="application/json")
 	public ResponseEntity <AccountHolder> createAccountHolder(@RequestBody AccountHolder accountHolder){
 		accountHolderServices.createAccountHolder(accountHolder);
--- src/main/java/com/moderncloudtech/financial/rest/TransferController.java
+++ src/main/java/com/moderncloudtech/financial/rest/TransferController.java
@@ -14,6 +14,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,19 +37,21 @@ public class TransferController {
 		this.transferServices = transferServices;
 		}
 	
-	
+	@CrossOrigin(origins="http://localhost:4200")
 	@RequestMapping(path="/Transfer", method=RequestMethod.GET)
 	public ResponseEntity<List<Transfer>> getAllTransfers(Model model) {
 		List<Transfer> transfers = transferServices.getAllTransfers();
 		return new ResponseEntity<List<Transfer>>(transfers, HttpStatus.OK);
 	}
 	
+	@CrossOrigin(origins="http://localhost:4200")
 	@RequestMapping(path="/Transfer/list", method=RequestMethod.GET, produces = "application/json" )
 	public ResponseEntity<List<Transfer>> getAllTransfers(){
 		List<Transfer> transfers = transferServices.getTransfers();
 		return new ResponseEntity<List<Transfer>>(transfers, HttpStatus.OK);
 	}
 	
+	@CrossOrigin(origins="http://localhost:4200")
 	@RequestMapping(path="/Transfer/d/{destination}", method=RequestMethod.GET)
 	public ResponseEntity<List<Transfer>> getByDestination(@PathVariable("destination") Long destination){
 		
@@ -57,6 +60,7 @@ public class TransferController {
 		
 	}
 	
+	@CrossOrigin(origins="http://localhost:4200")
 	@RequestMapping(path="/Transfer/s/{destination}", method=RequestMethod.GET)
 	public ResponseEntity<List<Transfer>> getBySource(@PathVariable("destination") Long destination, Sort sort){
 		sort = Sort.by(Sort.Direction.DESC, "source");
@@ -64,6 +68,7 @@ public class TransferController {
 		return new ResponseEntity<List<Transfer>>(transfers, HttpStatus.OK);
 	}
 	
+	@CrossOrigin(origins="http://localhost:4200")
 	@RequestMapping(path="/Transfer", method=RequestMethod.POST, consumes="application/json", produces="application/json")
 	public ResponseEntity<Transfer> createTransfer(@RequestBody Transfer transfer) throws IOException{
 		this.transferServices.create(transfer);
